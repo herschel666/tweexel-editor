a5cef7e5ecd420b112573445ffbf0613dc50c666
Emanuel Kluge <mail@emanuel-kluge.de>
2020-08-30 12:36:31 +0200

    test: check new URL behaviour

---
+++ src/pages/redirect-to-editor/redirect-to-editor.test.tsx
@@ -0,0 +1,39 @@
+import { h } from 'preact';
+import { route } from 'preact-router';
+import { render } from '@testing-library/preact';
+
+import { STORAGE_KEY_SIZE } from '../../constants';
+import { RedirectToEditor } from '.';
+
+jest.mock('preact-router', () => {
+  const pkg = jest.requireActual('preact-router');
+  const route = jest.fn();
+  return {
+    ...pkg,
+    route,
+  };
+});
+
+describe('redirecting to the default size', () => {
+  afterAll(() => jest.clearAllMocks());
+
+  it('redirects to the default size', () => {
+    render(<RedirectToEditor />);
+
+    expect(route).toHaveBeenCalledWith('/size/16-8/');
+  });
+});
+
+describe('redirecting to the stored size', () => {
+  beforeAll(() => {
+    sessionStorage[STORAGE_KEY_SIZE] = '[8, 16]';
+  });
+
+  afterAll(() => jest.clearAllMocks());
+
+  it('redirects to the default size', () => {
+    render(<RedirectToEditor />);
+
+    expect(route).toHaveBeenCalledWith('/size/8-16/');
+  });
+});
