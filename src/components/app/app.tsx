c3d79e01ba4f2bcbfb847f04212e0abf19c37986
Emanuel Kluge <mail@emanuel-kluge.de>
2020-11-04 21:07:38 +0100

    feat: add tracking

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,7 @@
-import { h } from 'preact';
-import type { FunctionalComponent } from 'preact';
+import { h, Component } from 'preact';
+import type { JSX } from 'preact';
 import { Router } from 'preact-router';
+import type { RouterOnChangeArgs } from 'preact-router';
 import classNames from 'classnames';
 
 import { EditorPage } from '../../pages/editor';
@@ -10,6 +11,10 @@ import { AboutPage } from '../../pages/about';
 import { FeedbackProvider } from '../feedback/';
 import { Header } from '../header';
 import { Footer } from '../footer';
+
+const TRACKING_PIXEL = 'https://analytics.e5l.de/cctv.gif';
+const TRACKING_ID =
+  'ff2bd199d3087d15dc9eef0b39947b0134f56fb97f80228606543eb7e456216b_ff606f0941ad6aab6e50abe4914df80a';
 
 const classMain = classNames(
   'flex',
@@ -31,19 +36,41 @@ const classMain = classNames(
 );
 const classContent = classNames('flex', 'flex-grow', 'mx-0');
 
-export const App: FunctionalComponent = () => (
-  <FeedbackProvider>
-    <div class={classMain}>
-      <Header />
-      <div class={classContent}>
-        <Router>
-          <RedirectToEditor path="/" />
-          <EditorPage path="/size/:size/" />
-          <AboutPage path="/about/" />
-          <NotFoundPage default={true} />
-        </Router>
-      </div>
-      <Footer />
-    </div>
-  </FeedbackProvider>
-);
+export class App extends Component {
+  track(pathname: string): void {
+    const { hostname } = location;
+    const resource = encodeURIComponent(pathname);
+    const referrer = encodeURIComponent(document.referrer);
+    const src = `${TRACKING_PIXEL}?id=${TRACKING_ID}&resource=${resource}&referrer=${referrer}`;
+    if (hostname === 'localhost') {
+      console.log('TRACKING "%s" refferred by "%s"', resource, referrer);
+    } else {
+      Object.assign(new Image(), { src });
+    }
+  }
+
+  handleRouteChange = ({ url, previous }: RouterOnChangeArgs): void => {
+    if (typeof document !== 'undefined' && url !== previous && url !== '/') {
+      this.track(url);
+    }
+  };
+
+  render(): JSX.Element {
+    return (
+      <FeedbackProvider>
+        <div class={classMain}>
+          <Header />
+          <div class={classContent}>
+            <Router onChange={this.handleRouteChange}>
+              <RedirectToEditor path="/" />
+              <EditorPage path="/size/:size/" />
+              <AboutPage path="/about/" />
+              <NotFoundPage default={true} />
+            </Router>
+          </div>
+          <Footer />
+        </div>
+      </FeedbackProvider>
+    );
+  }
+}
