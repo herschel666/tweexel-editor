5a075de7e19cd27a80e784748c85d9a77780256f
Emanuel Kluge <mail@emanuel-kluge.de>
2021-08-01 14:44:02 +0200

    chore: use correct test environment

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)(spec|test).[jt]s?(x)'],
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/tests/__mocks__/fileMock.js',
