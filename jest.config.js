a4adc9b45ac5273ca50207097b6df85c02d93a90
Emanuel Kluge <mail@emanuel-kluge.de>
2022-08-20 09:55:16 +0200

    chore: force CJS module resolution
    ...in the most ugly way

--- jest.config.js
+++ jest.config.js
@@ -17,10 +17,17 @@ module.exports = {
     '\\.(css|less|scss)$': 'identity-obj-proxy',
     '^./style$': 'identity-obj-proxy',
     '^preact$': '<rootDir>/node_modules/preact/dist/preact.min.js',
+    '^preact/test-utils$':
+      '<rootDir>/node_modules/preact/test-utils/dist/testUtils.umd.js',
+    '^preact/hooks$': '<rootDir>/node_modules/preact/hooks/dist/hooks.umd.js',
+    '^preact-router$':
+      '<rootDir>/node_modules/preact-router/dist/preact-router.umd.js',
     '^react$': 'preact-compat',
     '^react-dom$': 'preact-compat',
     '^create-react-class$': 'preact-compat/lib/create-react-class',
     '^react-addons-css-transition-group$': 'preact-css-transition-group',
+    '^@testing-library/preact$':
+      '<rootDir>/node_modules/@testing-library/preact/dist/cjs/index.js',
   },
   globals: {
     'ts-jest': {
